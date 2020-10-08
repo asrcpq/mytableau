804c818246b161b12bdf9c54f728129700398629
asrcpq <asrcpq@outlook.com>
2020-10-08 16:52:33 +0000

    Support word internally(not in parser)

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,16 @@ impl PropTree {
 		PropTree { nodes: Vec::new() }
 	}
 
-	fn atom_check(&self, ch: char, neg: bool) -> bool {
-		let lexical_unit = match self.nodes.last() {
-			Some(node) => node.lexical_unit,
+	fn atom_check(&self, name: &str, neg: bool) -> bool {
+		let lexical_unit = &match self.nodes.last() {
+			Some(node) => &node.lexical_unit,
 			None => return false,
 		};
 		match lexical_unit {
-			LexicalUnit::Atom(ch2) => neg && ch == ch2,
+			LexicalUnit::Atom(string) => neg && *name == *string,
 			LexicalUnit::Not(a) => {
-				if let LexicalUnit::Atom(ch2) = self.nodes[a].lexical_unit {
-					return ch == ch2;
+				if let LexicalUnit::Atom(string) = &self.nodes[*a].lexical_unit {
+					return !neg && *name == *string;
 				}
 				false
 			}
@@ -40,23 +40,23 @@ impl PropTree {
 	fn from_string(string: &str) -> PropTree {
 		let mut result: PropTree = PropTree { nodes: Vec::new() };
 		enum TokenOrUnit {
-			Token(char),
+			Token(String),
 			Unit(usize),
 		}
 		let mut token_stack: VecDeque<TokenOrUnit> = VecDeque::new();
 		for each_char in string.chars() {
 			match each_char {
 				'a'..='z' | '(' | '&' | '|' | '!' | '>' | '=' => {
-					token_stack.push_back(TokenOrUnit::Token(each_char))
+					token_stack.push_back(TokenOrUnit::Token(String::from(each_char)))
 				}
 				')' => {
 					let mut id_list = Vec::new();
 					loop {
 						let tou = token_stack.pop_back().unwrap();
 						match tou {
-							TokenOrUnit::Token('(') => break,
-							TokenOrUnit::Token(ch) => {
-								id_list.push(result.push_node(LexicalUnit::Atom(ch)));
+							TokenOrUnit::Token(ref string) if *string == "(" => break,
+							TokenOrUnit::Token(string) => {
+								id_list.push(result.push_node(LexicalUnit::Atom(string)));
 							}
 							TokenOrUnit::Unit(id) => {
 								id_list.push(id);
@@ -64,18 +64,20 @@ impl PropTree {
 						}
 					}
 					let new_id = match token_stack.pop_back().unwrap() {
-						TokenOrUnit::Token('&') => {
+						TokenOrUnit::Token(ref string) if string == "&" => {
 							result.push_node(LexicalUnit::And(id_list[1], id_list[0]))
 						}
-						TokenOrUnit::Token('|') => {
+						TokenOrUnit::Token(ref string) if string == "|" => {
 							result.push_node(LexicalUnit::Or(id_list[1], id_list[0]))
 						}
-						TokenOrUnit::Token('!') => result.push_node(LexicalUnit::Not(id_list[0])),
-						TokenOrUnit::Token('>') => {
+						TokenOrUnit::Token(ref string) if string == "!" => {
+							result.push_node(LexicalUnit::Not(id_list[0]))
+						}
+						TokenOrUnit::Token(ref string) if string == ">" => {
 							let id = result.push_node(LexicalUnit::Not(id_list[1]));
 							result.push_node(LexicalUnit::Or(id, id_list[0]))
 						}
-						TokenOrUnit::Token('=') => {
+						TokenOrUnit::Token(ref string) if string == "=" => {
 							let id1 = result.push_node(LexicalUnit::Not(id_list[1]));
 							let id1 = result.push_node(LexicalUnit::Or(id1, id_list[0]));
 							let id2 = result.push_node(LexicalUnit::Not(id_list[0]));
@@ -93,20 +95,20 @@ impl PropTree {
 	}
 
 	fn to_string_recurse(&self, id: usize) -> String {
-		match self.nodes[id].lexical_unit {
+		match &self.nodes[id].lexical_unit {
 			LexicalUnit::And(a, b) => {
 				"&(".to_string()
-					+ &self.to_string_recurse(a)
-					+ " " + &self.to_string_recurse(b)
+					+ &self.to_string_recurse(*a)
+					+ " " + &self.to_string_recurse(*b)
 					+ ")"
 			}
 			LexicalUnit::Or(a, b) => {
 				"|(".to_string()
-					+ &self.to_string_recurse(a)
-					+ " " + &self.to_string_recurse(b)
+					+ &self.to_string_recurse(*a)
+					+ " " + &self.to_string_recurse(*b)
 					+ ")"
 			}
-			LexicalUnit::Not(a) => "!".to_string() + &self.to_string_recurse(a),
+			LexicalUnit::Not(a) => "!".to_string() + &self.to_string_recurse(*a),
 			LexicalUnit::Atom(ch) => ch.to_string(),
 		}
 	}
@@ -127,7 +129,7 @@ impl PropTree {
 				let a1 = self.clone_subtree_recurse(new_tree, *a);
 				new_tree.push_node(LexicalUnit::Not(a1))
 			}
-			atom => new_tree.push_node(*atom),
+			atom => new_tree.push_node(atom.clone()),
 		}
 	}
 
@@ -174,12 +176,12 @@ struct PropTreeNode {
 	lexical_unit: LexicalUnit,
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Debug)]
 enum LexicalUnit {
 	And(usize, usize),
 	Or(usize, usize),
 	Not(usize),
-	Atom(char),
+	Atom(String),
 }
 
 struct TruthTree {
@@ -207,7 +209,7 @@ impl TruthTree {
 		id
 	}
 
-	fn upmatch(&self, id: usize, ch: char, neg: bool) -> bool {
+	fn upmatch(&self, id: usize, string: &str, neg: bool) -> bool {
 		let mut id: Option<usize> = Some(id);
 		loop {
 			id = self.nodes[id.unwrap()].parent;
@@ -215,7 +217,7 @@ impl TruthTree {
 				None => return false,
 				Some(id) => {
 					if let TruthTreeNodeData::Stable(prop_tree) = &self.nodes[id].data {
-						if prop_tree.atom_check(ch, neg) {
+						if prop_tree.atom_check(string, neg) {
 							return true;
 						}
 					} else {
@@ -254,59 +256,61 @@ impl TruthTree {
 
 		self.nodes[id].data = TruthTreeNodeData::Stable(prop_tree.clone());
 		// break last sentence
-		let lexical_unit = prop_tree.nodes.last().unwrap().lexical_unit;
-		let result = (|| match lexical_unit {
-			LexicalUnit::And(a, b) => {
-				let tree_a = prop_tree.clone_subtree(a);
-				let tree_b = prop_tree.clone_subtree(b);
-				leaf.push_back(tree_a);
-				leaf.push_back(tree_b);
+		let lexical_unit = &(&prop_tree.nodes.last().unwrap().lexical_unit);
+		let result = loop {
+			match lexical_unit {
+				LexicalUnit::And(a, b) => {
+					let tree_a = prop_tree.clone_subtree(*a);
+					let tree_b = prop_tree.clone_subtree(*b);
+					leaf.push_back(tree_a);
+					leaf.push_back(tree_b);
 
-				let id = self.push_node(id, TruthTreeNodeData::Leaf(leaf));
-				self.prove_recurse(id)
-			}
-			LexicalUnit::Or(a, b) => {
-				let tree_a = prop_tree.clone_subtree(a);
-				let tree_b = prop_tree.clone_subtree(b);
-				let mut leaf_a = leaf.clone();
-				leaf_a.push_back(tree_a);
-				let mut leaf_b = leaf;
-				leaf_b.push_back(tree_b);
-
-				let id_a = self.push_node(id, TruthTreeNodeData::Leaf(leaf_a));
-				if !self.prove_recurse(id_a) {
-					return false;
+					let id = self.push_node(id, TruthTreeNodeData::Leaf(leaf));
+					break self.prove_recurse(id);
 				}
-				let id_b = self.push_node(id, TruthTreeNodeData::Leaf(leaf_b));
-				self.prove_recurse(id_b)
-			}
-			LexicalUnit::Not(a) => {
-				let lexical_unit = prop_tree.nodes[a].lexical_unit;
+				LexicalUnit::Or(a, b) => {
+					let tree_a = prop_tree.clone_subtree(*a);
+					let tree_b = prop_tree.clone_subtree(*b);
+					let mut leaf_a = leaf.clone();
+					leaf_a.push_back(tree_a);
+					let mut leaf_b = leaf;
+					leaf_b.push_back(tree_b);
 
-				match lexical_unit {
-					LexicalUnit::Atom(ch) => {
-						if self.upmatch(id, ch, true) {
-							return true;
+					let id_a = self.push_node(id, TruthTreeNodeData::Leaf(leaf_a));
+					if !self.prove_recurse(id_a) {
+						break false;
+					}
+					let id_b = self.push_node(id, TruthTreeNodeData::Leaf(leaf_b));
+					break self.prove_recurse(id_b);
+				}
+				LexicalUnit::Not(a) => {
+					let lexical_unit = &(&prop_tree.nodes[*a].lexical_unit);
+
+					match lexical_unit {
+						LexicalUnit::Atom(string) => {
+							if self.upmatch(id, string, true) {
+								break true;
+							}
+						}
+						_ => {
+							prop_tree.nodes.pop();
+							prop_tree = prop_tree.negate();
+							leaf.push_back(prop_tree);
 						}
 					}
-					_ => {
-						prop_tree.nodes.pop();
-						prop_tree = prop_tree.negate();
-						leaf.push_back(prop_tree);
+					let id = self.push_node(id, TruthTreeNodeData::Leaf(leaf));
+					break self.prove_recurse(id);
+				}
+				LexicalUnit::Atom(string) => {
+					if self.upmatch(id, string, true) {
+						break true;
 					}
-				}
-				let id = self.push_node(id, TruthTreeNodeData::Leaf(leaf));
-				self.prove_recurse(id)
-			}
-			LexicalUnit::Atom(ch) => {
-				if self.upmatch(id, ch, true) {
-					return true;
-				}
 
-				let id = self.push_node(id, TruthTreeNodeData::Leaf(leaf));
-				self.prove_recurse(id)
+					let id = self.push_node(id, TruthTreeNodeData::Leaf(leaf));
+					break self.prove_recurse(id);
+				}
 			}
-		})();
+		};
 		unsafe {
 			INDENT.pop();
 			println!("{}{}", INDENT, result);
